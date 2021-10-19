5d8968f806bfae98b66a07cf88b9d3102a666e8a
sivasankar-3355 <shankarsiva608@gmail.com>
2021-10-19 14:38:00 +0530

    first comit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD_GmzKtNqPkJEhRqAfO1oLzHaP4CAFrFQ',
+  authDomain: 'linkedin-clone-96dad.firebaseapp.com',
+  projectId: 'linkedin-clone-96dad',
+  storageBucket: 'linkedin-clone-96dad.appspot.com',
+  messagingSenderId: '771906766241',
+  appId: '1:771906766241:web:1afc844933d6e192d777e0',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
